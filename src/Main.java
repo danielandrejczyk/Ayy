705a2c6724d8e8353e5fcee730be9abcf2e4cabc
andre <andre@DESKTOP-GTKC47B>
2017-09-22 20:41:04 -0400

    Window pops up

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+//Step 1: Display a blank window
 
 public class Main {
 
+Main(){
+		
+		JFrame frame = new JFrame("Box Test"); //Title in parenthesis
+		frame.setLayout(new FlowLayout());
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}	
 }
