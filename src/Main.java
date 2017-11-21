c7fb30be943e6044a2ef2b4f146580ea136a4c54
andre <andre@DESKTOP-GTKC47B>
2017-11-21 12:19:32 -0500

    blah

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 
 //Step 1: Display a blank window
 
-public class Main {
+public class Main extends Canvas {
 
-Main(){
-		
+	Dimension screenSize = new Dimension(500, 500);
+
+	Main(){
+
 		JFrame frame = new JFrame("Box Test"); //Title in parenthesis
 		frame.setLayout(new FlowLayout());
-		frame.setSize(500,500);
+		frame.setSize(screenSize);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}	
